457d947cf8afc43deb80037b5626290c38aaa4ea
unknown <nguyenduc392004@gmail.com>
2025-06-05 06:43:48 -0700

    update-duties-services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom"
-import LoginPage from './pages/common/LoginPage/LoginPage'
+import LoginPage from './pages/auth/LoginPage/LoginPage'
 import CharacterMix from './pages/lobby/CharacterMix/CharacterMix'
 import Lobby from "./pages/lobby/Lobby/Lobby"
-
+import GachaMachine from "./Test/Test"
 
 function App() {
 
@@ -12,6 +12,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/chacracterMix" element={<CharacterMix />} />
         <Route path="/lobby" element={<Lobby />} />
+        <Route path="/test" element={<GachaMachine />} />
+
       </Routes>
 
     </>
